83f808c049021a08ab870f9e1d562aec4b82058e
Ivan Lee <44192990+ivan-zynesis@users.noreply.github.com>
2024-04-21 16:06:15 +0800

    added endpoints for getting reporting data (5a and 5b)

--- apps/api/src/controllers/reporting/controller.ts
+++ apps/api/src/controllers/reporting/controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { TimeSeriesMostQueriedResponse } from './dto';
+import {
+  RecentQueriedParameterResponse,
+  TimeSeriesMostQueriedResponse,
+  TopQueriedParameterResponse,
+} from './dto';
 import { TimeSeriesQueryAnalytic } from '../../modules/reporting/providers/TimeSeriesQueryAnalytic';
+import { TimeSeriesQueryEntity } from '../../entities/TimeSeriesQuery';
+import { RecentQueriesAnalytic } from '../../modules/reporting/providers/RecentQueriesAnalytic';
+import {
+  RecentQueriesAnalyticService,
+  TimeSeriesQueryAnalyticService,
+  TopQueriesAnalyticService,
+} from '../../modules/reporting/module';
+import { TopQueriesAnalytic } from '../../modules/reporting/providers/TopQueriesAnalytic';
 
 @ApiTags('Reporting')
 @Controller('reporting')
 export class ReportingController {
-  constructor(private readonly timeSeries: TimeSeriesQueryAnalytic) {}
+  constructor(
+    @Inject(TimeSeriesQueryAnalyticService)
+    private readonly timeSeriesAnalytic: TimeSeriesQueryAnalytic,
+    @Inject(RecentQueriesAnalyticService)
+    private readonly recentQueriesAnalytic: RecentQueriesAnalytic,
+    @Inject(TopQueriesAnalyticService)
+    private readonly topQueriesAnalytic: TopQueriesAnalytic,
+  ) {}
 
   @ApiQuery({
     name: 'start',
@@ -19,23 +38,61 @@ export class ReportingController {
     description: 'date time iso string',
   })
   @ApiResponse({ type: TimeSeriesMostQueriedResponse })
-  @Get('/time-series/most-queried')
+  @Get('/most-queried/traffic-camera-datetime')
   async highestQueriedParamWithMovingOneHourBlock(
     @Query('start') start: string,
     @Query('end') end: string,
   ): Promise<TimeSeriesMostQueriedResponse> {
     const onlySupportedTypeAtm = 'traffic-cam';
-    const mostQueriedTimestamp = await this.timeSeries.selectMostQueried(
-      onlySupportedTypeAtm,
-      start,
-      end,
-    );
+    const mostQueriedTimestamp =
+      await this.timeSeriesAnalytic.selectMostQueried(
+        onlySupportedTypeAtm,
+        start,
+        end,
+      );
     return {
       type: onlySupportedTypeAtm,
       selectedDateTime: mostQueriedTimestamp,
     };
   }
 
-  // TODO: recent queries
+  @ApiResponse({ type: TimeSeriesMostQueriedResponse })
+  @Get('/recent/traffic-camera-datetime')
+  async recentQueries(): Promise<RecentQueriedParameterResponse> {
+    const onlySupportedTypeAtm = TimeSeriesQueryEntity;
+    const mostQueriedTimestamps =
+      await this.recentQueriesAnalytic.getRecentQueries(
+        onlySupportedTypeAtm,
+        'selectedDateTime',
+      );
+    return {
+      queriedParameters: mostQueriedTimestamps.map((ts) => ({
+        queriedParameterValue: ts.selectedDateTime.toISOString(),
+        createdAt: ts.createdAt,
+      })),
+    };
+  }
+
+  @ApiResponse({ type: TopQueriedParameterResponse })
+  @Get('/top/traffic-camera-datetime')
+  async topQueries(
+    @Query('start') start: string,
+    @Query('end') end: string,
+  ): Promise<TopQueriedParameterResponse> {
+    const onlySupportedTypeAtm = TimeSeriesQueryEntity;
+    const mostQueriedTimestamps = await this.topQueriesAnalytic.getTopQueries(
+      onlySupportedTypeAtm,
+      'selectedDateTime',
+      start,
+      end,
+    );
+    return {
+      topQueriedParameters: mostQueriedTimestamps.map((ts) => ({
+        queriedParameterValue: ts.selectedDateTime.toISOString(),
+        count: ts.count,
+      })),
+    };
+  }
+
   // TODO: top 10 queries params
 }
